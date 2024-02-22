bc8ec4aeffbb34b03e2c283c6a40ad9c967a02a9
María Del Mar <132160793+MarSierraG@users.noreply.github.com>
2024-02-22 23:59:46 +0100

    Add files via upload

---
+++ UD03.4.Boletin/UD03.4.Bolet-n_de_ejercicios_JUnit/UD03.4.BoletinJUnit/src/main/java/ies/sotero/hernandez/daw/endes/UD03/BoletinJUnit/NoSuchItemException.java
@@ -0,0 +1,8 @@
+package ies.sotero.hernandez.daw.endes.UD03.BoletinJUnit;
+
+public class NoSuchItemException extends Exception {
+	public NoSuchItemException(String message) {
+		super(message);
+	}
+}
+
