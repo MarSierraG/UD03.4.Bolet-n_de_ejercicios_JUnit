9216965a1aa3f17caf3ea649c5cd5f51af0b44b4
María Del Mar <132160793+MarSierraG@users.noreply.github.com>
2024-02-23 11:00:36 +0100

    Delete UD03.4.Boletin directory

--- UD03.4.Boletin/src/main/java/ies/sotero/hernandez/daw/endes/UD03/BoletinJUnit/NoSuchItemException.java
+++
@@ -1,8 +0,0 @@
-package ies.sotero.hernandez.daw.endes.UD03.BoletinJUnit;
-
-public class NoSuchItemException extends Exception {
-	public NoSuchItemException(String message) {
-		super(message);
-	}
-}
-
--- UD03.4.Boletin/src/test/java/ies/sotero/hernandez/daw/endes/UD03/BoletinJUnit/AllTests.java
+++
@@ -1,9 +0,0 @@
-package ies.sotero.hernandez.daw.endes.UD03.BoletinJUnit;
-import org.junit.platform.suite.api.SelectClasses;
-import org.junit.platform.suite.api.Suite;
-
-@Suite
-@SelectClasses({BoaTest.class, FridgeTest.class, SubscripcionTest.class, PilaTest.class})
-public class AllTests {
-
-}
